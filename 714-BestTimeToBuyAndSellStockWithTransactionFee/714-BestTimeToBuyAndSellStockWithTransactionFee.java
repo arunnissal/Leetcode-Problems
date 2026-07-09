438907bd093a24ea87c66baec16e0871dce3bd0d
arunnissal <arunnissal45@gmail.com>
2026-07-09 10:13:51 +0530

    Create 714-BestTimeToBuyAndSellStockWithTransactionFee

---
+++ 714-BestTimeToBuyAndSellStockWithTransactionFee/714-BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 10:13:52 AM
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int buy = Integer.MIN_VALUE;
+        int sell = 0;
+
+        for (int price : prices) {
+            buy = Math.max(buy, sell - price);
+            sell = Math.max(sell, buy + price - fee);
+        }
+
+        return sell;
+    }
+}
