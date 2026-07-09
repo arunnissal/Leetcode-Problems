5419bb60fb02f51feff13ca5ade20bcbccc22be6
arunnissal <arunnissal45@gmail.com>
2026-07-09 10:13:34 +0530

    Create 1039-FindTheTownJudge

---
+++ 1039-FindTheTownJudge/1039-FindTheTownJudge.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 10:13:34 AM
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] outDegree=new int[n + 1];
+        int[] inDegree=new int[n + 1];
+        for (int[] t : trust) {
+            outDegree[t[0]]++;
+            inDegree[t[1]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (outDegree[i] == 0 && inDegree[i] == n - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
