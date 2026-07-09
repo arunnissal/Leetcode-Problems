313246b96e8daf79482caf7c662e8382be42d6bb
arunnissal <arunnissal45@gmail.com>
2026-07-09 10:14:04 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.py
@@ -0,0 +1,15 @@
+# Last updated: 7/9/2026, 10:14:04 AM
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        if num <= 1:
+            return False
+        
+        total = 1
+        
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                total += i
+                if i != num // i:
+                    total += num // i
+        
+        return total == num
