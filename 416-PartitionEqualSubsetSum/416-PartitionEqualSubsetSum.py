0f96e773eb24de295eab1a24c640103ba926fec0
arunnissal <arunnissal45@gmail.com>
2026-07-09 10:14:10 +0530

    Create 416-PartitionEqualSubsetSum

---
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
@@ -0,0 +1,20 @@
+# Last updated: 7/9/2026, 10:14:10 AM
+class Solution:
+    def canPartition(self, nums):
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        
+        target = total // 2
+        possible_sums = set([0])
+        
+        for num in nums:
+            next_sums = set()
+            for s in possible_sums:
+                if s + num == target:
+                    return True
+                next_sums.add(s + num)
+                next_sums.add(s)
+            possible_sums = next_sums
+        
+        return target in possible_sums
