fe659bdd7b4d90f23b5b27741626f2d03826bcb4
arunnissal <arunnissal45@gmail.com>
2026-07-09 10:15:53 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -0,0 +1,11 @@
+# Last updated: 7/9/2026, 10:15:53 AM
+class Solution(object):
+    def maxSubArray(self, nums):
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        max_sum = dp[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(nums[i], nums[i] + dp[i-1])
+            max_sum = max(max_sum, dp[i])
+        return max_sum
+        
