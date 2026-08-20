d1630916bc8224c9cf907ddd853da83fe948745f
ARUNNISSAL B <arunnissal45@gmail.com>
2026-08-20 15:03:01 +0530

    File updated at 8/20/2026, 3:03:04 PM

--- 3033-ModifytheMatrix/3033-ModifytheMatrix.java
+++ 3033-ModifytheMatrix/3033-ModifytheMatrix.java
@@ -1,13 +1,14 @@
-// Last updated: 8/20/2026, 2:56:33 PM
+// Last updated: 8/20/2026, 3:03:04 PM
 1class Solution {
-2    public boolean judgeCircle(String moves) {
-3        int x=0,y=0;
-4        for(char c:moves.toCharArray()){
-5            if(c=='U') x++;
-6            else if(c=='D') x--;
-7            else if(c=='L') y++;
-8            else y--;
-9        }
-10        return x==0 && y==0;
-11    }
-12}
+2    public int removeDuplicates(int[] nums) {
+3        int i=0,j=1;
+4        while(j<nums.length){
+5            if(nums[i]!=nums[j]){
+6                i++;
+7                nums[i]=nums[j];
+8            }
+9            j++;
+10        }
+11        return i+1;
+12    }
+13}
