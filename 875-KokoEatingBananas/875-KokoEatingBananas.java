b52a3bbd43f0cd807ecd022b5e692affa50834e9
ARUNNISSAL B <arunnissal45@gmail.com>
2026-08-20 21:38:20 +0530

    File updated at 8/20/2026, 9:38:23 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.java
+++ 875-KokoEatingBananas/875-KokoEatingBananas.java
@@ -1,19 +1,21 @@
-// Last updated: 8/20/2026, 9:37:52 PM
+// Last updated: 8/20/2026, 9:38:23 PM
 1class Solution {
 2    public int minEatingSpeed(int[] piles, int h) {
 3        int left=1,right=0;
 4        for(int pile:piles){
 5            right=Math.max(right,pile);
 6        }
-7        while(left<right){
-8            int mid=(left+right)/2;
-9            long hours=0;
-10            for(int pile:piles){
-11                hours+=(pile+mid-1)/mid;
-12            }
-13            if(hours<=h) right=mid;
-14            else left=mid+1;
-15        }
-16        return left;
-17    }
-18}
+7        
+8        while(left<right){
+9            int mid=(left+right)/2;
+10            long hours=0;
+11            for(int pile:piles){
+12                hours+=(pile+mid-1)/mid;
+13            }
+14            if(hours<=h) right=mid;
+15            else left=mid+1;
+16
+17        }
+18        return left;
+19    }
+20}
