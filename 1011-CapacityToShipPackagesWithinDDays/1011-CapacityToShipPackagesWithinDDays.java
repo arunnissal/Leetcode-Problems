db9d656b2676dd7b153434b7ef6c1e893b3e0897
ARUNNISSAL B <arunnissal45@gmail.com>
2026-08-20 21:45:44 +0530

    Create 1011-CapacityToShipPackagesWithinDDays

---
+++ 1011-CapacityToShipPackagesWithinDDays/1011-CapacityToShipPackagesWithinDDays.java
@@ -0,0 +1,29 @@
+// Last updated: 8/20/2026, 9:45:47 PM
+1class Solution {
+2    public int shipWithinDays(int[] weights, int days) {
+3        int left=0,right=0;
+4        for(int i:weights){
+5            left=Math.max(left,i);
+6            right+=i;
+7        }
+8        while(left<right){
+9            int mid=(left+right)/2;
+10            int day=1;
+11            int cur=0;
+12            for(int weigh:weights){
+13                if(cur+weigh>mid){
+14                    cur=weigh;
+15                    day++;
+16                }else{
+17                    cur+=weigh;
+18                }
+19            }
+20            if(day<=days){
+21                right=mid;
+22            }else{
+23                left=mid+1;
+24            }
+25        }
+26        return left;
+27    }
+28}
