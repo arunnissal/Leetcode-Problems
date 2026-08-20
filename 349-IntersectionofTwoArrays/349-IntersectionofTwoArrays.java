2c96535061a264d6e88ad6bc4bfe2bd10aed956f
ARUNNISSAL B <arunnissal45@gmail.com>
2026-08-20 18:24:47 +0530

    File updated at 8/20/2026, 6:24:50 PM

--- 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
@@ -1,24 +1,13 @@
-// Last updated: 8/20/2026, 6:00:05 PM
-1import java.util.Set;
-2
-3class Solution {
-4    public int[] intersection(int[] nums1, int[] nums2) {
-5        Set<Integer> set1 = new HashSet<>();
-6        for (int i : nums1) {
-7            set1.add(i);
-8        }
-9        Set<Integer> ans=new HashSet<>();
-10        for(int i:nums2){
-11            if(set1.contains(i)){
-12                ans.add(i);
-13            }
-14        }
-15        int[] arr=new int[ans.size()];
-16        int j=0;
-17        for(Integer i:ans){
-18            arr[j]=i.intValue();
-19            j++;
-20        }
-21        return arr;
-22    }
-23}
+// Last updated: 8/20/2026, 6:24:50 PM
+1class Solution {
+2    public int maxSubArray(int[] nums) {
+3        int current=0;
+4        int maxSum=nums[0];
+5        for(int i:nums){
+6            if(current<0) current=0;
+7            current+=i;
+8            maxSum=Math.max(maxSum,current);
+9        }
+10        return maxSum;
+11    }
+12}
