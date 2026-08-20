5b9a85a10fcde443f33600b8e21b0da08196c7f8
ARUNNISSAL B <arunnissal45@gmail.com>
2026-08-20 15:14:09 +0530

    File updated at 8/20/2026, 3:14:12 PM

--- 3033-ModifytheMatrix/3033-ModifytheMatrix.java
+++ 3033-ModifytheMatrix/3033-ModifytheMatrix.java
@@ -1,14 +1,23 @@
-// Last updated: 8/20/2026, 3:08:23 PM
+// Last updated: 8/20/2026, 3:14:12 PM
 1class Solution {
-2    public int removeElement(int[] nums, int val) {
-3        int i=0,j=0;
-4        while(j<nums.length){
-5            if(nums[j]!=val){
-6                nums[i]=nums[j];
-7                i++;
-8            }
-9            j++;
-10        }
-11        return i;
-12    }
-13}
+2    public void merge(int[] a, int m, int[] b, int n) {
+3        int i=m-1,j=n-1,k=m+n-1;
+4        while(i>=0 && j>=0){
+5            if(a[i]>b[j]){
+6                a[k]=a[i];
+7                i--;
+8            }else{
+9                a[k]=b[j];
+10                j--;
+11            }
+12            k--;
+13        }
+14        while(j>=0){
+15            a[k]=b[j];
+16            j--;
+17            k--;
+18        }
+19
+20    }
+21}
+22
