30750171e38a0b8a69edb506ebc954d179bb6ef2
ARUNNISSAL B <arunnissal45@gmail.com>
2026-08-20 21:02:22 +0530

    File updated at 8/20/2026, 9:02:25 PM

--- 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
@@ -1,13 +1,37 @@
-// Last updated: 8/20/2026, 6:24:50 PM
+// Last updated: 8/20/2026, 9:02:25 PM
 1class Solution {
-2    public int maxSubArray(int[] nums) {
-3        int current=0;
-4        int maxSum=nums[0];
-5        for(int i:nums){
-6            if(current<0) current=0;
-7            current+=i;
-8            maxSum=Math.max(maxSum,current);
-9        }
-10        return maxSum;
-11    }
-12}
+2    public int trap(int[] height) {
+3        int left = 0;
+4        int right = height.length - 1;
+5
+6        int leftMax = 0;
+7        int rightMax = 0;
+8        int water = 0;
+9
+10        while (left < right) {
+11
+12            if (height[left] < height[right]) {
+13
+14                if (height[left] >= leftMax) {
+15                    leftMax = height[left];
+16                } else {
+17                    water += leftMax - height[left];
+18                }
+19
+20                left++;
+21
+22            } else {
+23
+24                if (height[right] >= rightMax) {
+25                    rightMax = height[right];
+26                } else {
+27                    water += rightMax - height[right];
+28                }
+29
+30                right--;
+31            }
+32        }
+33
+34        return water;
+35    }
+36}
