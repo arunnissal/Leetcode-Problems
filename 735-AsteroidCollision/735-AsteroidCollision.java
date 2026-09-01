726ee39306e342bb1b6645a3cb344f02736e498c
ARUNNISSAL B <arunnissal45@gmail.com>
2026-09-01 14:25:14 +0530

    File updated at 9/1/2026, 2:25:12 PM

--- 735-AsteroidCollision/735-AsteroidCollision.java
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -1,40 +1,30 @@
-// Last updated: 8/28/2026, 10:19:24 AM
+// Last updated: 9/1/2026, 2:25:12 PM
 1class Solution {
 2    public int[] asteroidCollision(int[] asteroids) {
-3
-4        Stack<Integer> st = new Stack<>();
-5
-6        for(int i : asteroids) {
-7
-8            boolean alive = true;
-9
-10            while(alive && !st.isEmpty() && st.peek() > 0 && i < 0) {
-11
-12                if(st.peek() < -i) {
-13                    st.pop();
+3        Stack<Integer> st = new Stack<>();
+4        for(int i:asteroids){
+5            boolean alive=true;
+6            while(alive && !st.isEmpty() && st.peek()>0 && i<0){
+7                if(st.peek()<-i){
+8                    st.pop();
+9                }else if(st.peek()==-i){
+10                    st.pop();
+11                    alive=false;
+12                }else{
+13                    alive=false;
 14                }
-15                else if(st.peek() == -i) {
-16                    st.pop();
-17                    alive = false;
-18                }
-19                else {
-20                    alive = false;
-21                }
-22            }
-23
-24            if(alive) {
-25                st.push(i);
-26            }
-27        }
-28
-29        int[] arr = new int[st.size()];
-30        int a = 0;
-31
-32        for(Integer i : st) {
-33            arr[a] = i;
-34            a++;
-35        }
-36
-37        return arr;
-38    }
-39}
+15
+16            }
+17            if(alive){
+18                st.push(i);
+19            }
+20        }
+21        int[] arr=new int[st.size()];
+22        int a=0;
+23        for(int i:st){
+24            arr[a]=i;
+25            a++;
+26        }
+27        return arr;
+28    }
+29}
