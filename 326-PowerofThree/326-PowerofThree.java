2979fb160de3b7234b5c14bf5c12052496ad7af6
ARUNNISSAL B <arunnissal45@gmail.com>
2026-09-02 09:27:52 +0530

    File updated at 9/2/2026, 9:27:52 AM

--- 326-PowerofThree/326-PowerofThree.java
+++ 326-PowerofThree/326-PowerofThree.java
@@ -1,12 +1,22 @@
-// Last updated: 9/2/2026, 8:58:22 AM
+// Last updated: 9/2/2026, 9:27:52 AM
 1class Solution {
-2    public boolean isPowerOfThree(int n) {
-3        if (n<=0){
-4            return false;
-5        }
-6        while(n%3==0){
-7            n=n/3;
-8        }
-9        return n==1;
-10    }
-11}
+2    public int evalRPN(String[] tokens) {
+3        int n = tokens.length;
+4        int[] st = new int[n];
+5        int top=-1;
+6        for(String t:tokens){
+7            if(t.equals("+")) {
+8                st[top-1]=st[top-1]+st[top--];
+9            }else if(t.equals("-")) {
+10                st[top-1]=st[top-1]-st[top--];
+11            }else if(t.equals("*")) {
+12                st[top-1]=st[top-1]*st[top--];
+13            }else if(t.equals("/")) {
+14                st[top-1]=st[top-1]/st[top--];
+15            }else {
+16                st[++top]=Integer.parseInt(t);
+17            }
+18        }
+19        return st[0];
+20    }
+21}
