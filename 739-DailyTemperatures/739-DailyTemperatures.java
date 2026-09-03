161c06f8ce082befcb6a968278946199ff03d236
ARUNNISSAL B <arunnissal45@gmail.com>
2026-09-03 09:24:43 +0530

    File updated at 9/3/2026, 9:24:43 AM

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,16 +1,21 @@
-// Last updated: 8/28/2026, 2:10:27 PM
+// Last updated: 9/3/2026, 9:24:43 AM
 1class Solution {
-2    public int[] dailyTemperatures(int[] temperatures) {
+2    public int largestRectangleArea(int[] heights) {
 3        Stack<Integer> st=new Stack<>();
-4        int n=temperatures.length;
-5        int[] arr=new int[n];
-6        for(int i=0;i<n;i++){
-7            while(!st.isEmpty() && temperatures[i]>temperatures[st.peek()]){
-8                int index=st.pop();
-9                arr[index]=i-index;
-10            }
-11            st.push(i);
-12        }
-13        return arr;
-14    }
-15}
+4        int max=0;
+5        for(int i=0;i<=heights.length;i++){
+6            int current;
+7            if(i==heights.length) current=0;
+8            else current=heights[i];
+9            while(!st.isEmpty() && current<heights[st.peek()]){
+10                int h=heights[st.pop()];
+11                int width;
+12                if(st.isEmpty()) width=i;
+13                else width=i-st.peek()-1;
+14                max=Math.max(max,h*width);
+15            }
+16            st.push(i);
+17        }
+18        return max;
+19    }
+20}
