14e7bb827fa2054ec7f71231a4eba3afee5f2cca
ARUNNISSAL B <arunnissal45@gmail.com>
2026-09-08 09:05:35 +0530

    File updated at 9/8/2026, 9:05:36 AM

--- 3870-CountCommasinRange/3870-CountCommasinRange.java
+++ 3870-CountCommasinRange/3870-CountCommasinRange.java
@@ -1,7 +1,6 @@
-// Last updated: 9/8/2026, 9:03:23 AM
+// Last updated: 9/8/2026, 9:05:36 AM
 1class Solution {
 2    public int countCommas(int n) {
-3        if(n<1000) return 0;
-4        return n-999;
-5    }
-6}
+3        return n>=1000 ? n-999 : 0;
+4    }
+5}
