8f1ebc996cdb03d7b4212171d36ef48a7d9a8c23
ARUNNISSAL B <arunnissal45@gmail.com>
2026-09-08 09:08:39 +0530

    File updated at 9/8/2026, 9:08:39 AM

--- 3870-CountCommasinRange/3870-CountCommasinRange.java
+++ 3870-CountCommasinRange/3870-CountCommasinRange.java
@@ -1,6 +1,9 @@
-// Last updated: 9/8/2026, 9:05:36 AM
-1class Solution {
-2    public int countCommas(int n) {
-3        return n>=1000 ? n-999 : 0;
-4    }
-5}
+// Last updated: 9/8/2026, 9:08:39 AM
+class Solution {
+    public int countCommas(int n) {
+        if(n<1000){
+            return 0;
+        }
+        return n - 999;
+    }
+}
